77eb0c5dbf35ca58397351b58aaff72a9774c93f
stephyang <stephanie1635945@gmail.com>
2016-05-05 10:38:19 +0800

    Initial Commit

---
+++ countTextnumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countTextnumber
+//
+//  Created by stephanie yang on 2016/5/5.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
